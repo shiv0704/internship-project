e91a2a1e2c945837e0c62d8fc0c306e5d752521b
shiv0704 <147787187+shiv0704@users.noreply.github.com>
2023-11-20 13:15:19 +1300

    Add files via upload

---
+++ applicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using WebApplication6.Data;
+
+internal class applicationDbContext
+{
+    public object Categories { get; internal set; }
+
+    public static implicit operator applicationDbContext(ApplicationDbContext v)
+    {
+        throw new NotImplementedException();
+    }
+}
